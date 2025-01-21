ead79a1258bc046527b095064ac832d0000e0d77
Hashual <145881009+Hashual@users.noreply.github.com>
2025-01-21 19:54:18 +0100

    start creating the voting process

---
+++ target/debug/deps/windows_targets-a8d7d374b366c185.d
@@ -0,0 +1,7 @@
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\target\debug\deps\libwindows_targets-a8d7d374b366c185.rmeta: C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows-targets-0.52.6\src/lib.rs
+
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\target\debug\deps\libwindows_targets-a8d7d374b366c185.rlib: C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows-targets-0.52.6\src/lib.rs
+
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\target\debug\deps\windows_targets-a8d7d374b366c185.d: C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows-targets-0.52.6\src/lib.rs
+
+C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows-targets-0.52.6\src/lib.rs:
