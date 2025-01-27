6416a6dc2fa1a40f9b46383d3514dca0693b9b46
Hashual <145881009+Hashual@users.noreply.github.com>
2025-01-27 13:15:59 +0100

    Part 1.4 FROM TP2

---
+++ v020_configuration/target/debug/deps/clap-22364707c76d9f5b.d
@@ -0,0 +1,9 @@
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\v020_configuration\target\debug\deps\libclap-22364707c76d9f5b.rmeta: C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src/lib.rs C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.rs C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.md
+
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\v020_configuration\target\debug\deps\libclap-22364707c76d9f5b.rlib: C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src/lib.rs C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.rs C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.md
+
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\v020_configuration\target\debug\deps\clap-22364707c76d9f5b.d: C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src/lib.rs C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.rs C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.md
+
+C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src/lib.rs:
+C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.rs:
+C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.md:
---
+++ v020_configuration/target/debug/deps/v020_configuration.d
@@ -0,0 +1,5 @@
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\v020_configuration\target\debug\deps\v020_configuration.exe: src/main.rs
+
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\v020_configuration\target\debug\deps\v020_configuration.d: src/main.rs
+
+src/main.rs:
