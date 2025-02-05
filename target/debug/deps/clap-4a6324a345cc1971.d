1f4f14211c72c616343546c3e7cf6904d6f1e86a
Hashual <145881009+Hashual@users.noreply.github.com>
2025-02-05 16:14:56 +0100

    add tests from TP 3

---
+++ target/debug/deps/clap-4a6324a345cc1971.d
@@ -0,0 +1,9 @@
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\target\debug\deps\libclap-4a6324a345cc1971.rmeta: C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src/lib.rs C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.rs C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.md
+
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\target\debug\deps\libclap-4a6324a345cc1971.rlib: C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src/lib.rs C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.rs C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.md
+
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\target\debug\deps\clap-4a6324a345cc1971.d: C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src/lib.rs C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.rs C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.md
+
+C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src/lib.rs:
+C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.rs:
+C:\Users\dodo2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.27\src\../examples/demo.md:
---
+++ target/debug/deps/v040_memory-b9b247e7c80f75f8.d
@@ -0,0 +1,13 @@
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\target\debug\deps\libv040_memory-b9b247e7c80f75f8.rmeta: v040_memory\src\lib.rs v040_memory\src\configuration.rs v040_memory\src\app_builder.rs v040_memory\src\domain.rs v040_memory\src\storage.rs v040_memory\src\storages\mod.rs v040_memory\src\storages\memory.rs
+
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\target\debug\deps\libv040_memory-b9b247e7c80f75f8.rlib: v040_memory\src\lib.rs v040_memory\src\configuration.rs v040_memory\src\app_builder.rs v040_memory\src\domain.rs v040_memory\src\storage.rs v040_memory\src\storages\mod.rs v040_memory\src\storages\memory.rs
+
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\target\debug\deps\v040_memory-b9b247e7c80f75f8.d: v040_memory\src\lib.rs v040_memory\src\configuration.rs v040_memory\src\app_builder.rs v040_memory\src\domain.rs v040_memory\src\storage.rs v040_memory\src\storages\mod.rs v040_memory\src\storages\memory.rs
+
+v040_memory\src\lib.rs:
+v040_memory\src\configuration.rs:
+v040_memory\src\app_builder.rs:
+v040_memory\src\domain.rs:
+v040_memory\src\storage.rs:
+v040_memory\src\storages\mod.rs:
+v040_memory\src\storages\memory.rs:
