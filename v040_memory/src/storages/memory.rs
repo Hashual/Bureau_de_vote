30e3027b5e0633847f52c04b000a840c8d27cc51
Hashual <loizeau.dorian05@gmail.com>
2025-02-05 16:31:07 +0100

    add Tests for TP4

--- v040_memory/src/storages/memory.rs
+++ v040_memory/src/storages/memory.rs
@@ -24,3 +24,22 @@ impl Storage for MemoryStore{
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{domain::{BallotPaper, Candidate, Voter, VotingMachine}, storage::Storage, storages::memory::MemoryStore};
+
+    #[tokio::test]
+    async fn verifie_que_la_machine_du_get_est_la_meme_que_le_put(){
+        let candidats = vec![Candidate("A".to_string()), Candidate("B".to_string())];
+        let mut machine = VotingMachine::new(candidats);
+        let mut memory = MemoryStore::new(machine.clone()).await.unwrap();
+        let machine2 = memory.get_voting_machine().await.unwrap();
+        assert_eq!(machine, machine2);
+        machine.vote(BallotPaper{voter: Voter("v1".to_string()), candidate: Some(Candidate("A".to_string()))});
+        memory.put_voting_machine(machine.clone()).await.unwrap();
+        let machine3 = memory.get_voting_machine().await.unwrap();
+        assert_eq!(machine, machine3);
+
+    }
+}
