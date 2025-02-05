e752f0b6189c38fe75f41bd13d96a73b3334251e
Hashual <loizeau.dorian05@gmail.com>
2025-02-05 16:14:56 +0100

    add tests from TP 3

---
+++ target/debug/deps/v040_memory-75a701753bb7c920.d
@@ -0,0 +1,11 @@
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\target\debug\deps\v040_memory-75a701753bb7c920.exe: v040_memory\src\lib.rs v040_memory\src\configuration.rs v040_memory\src\app_builder.rs v040_memory\src\domain.rs v040_memory\src\storage.rs v040_memory\src\storages\mod.rs v040_memory\src\storages\memory.rs
+
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\target\debug\deps\v040_memory-75a701753bb7c920.d: v040_memory\src\lib.rs v040_memory\src\configuration.rs v040_memory\src\app_builder.rs v040_memory\src\domain.rs v040_memory\src\storage.rs v040_memory\src\storages\mod.rs v040_memory\src\storages\memory.rs
+
+v040_memory\src\lib.rs:
+v040_memory\src\configuration.rs:
+v040_memory\src\app_builder.rs:
+v040_memory\src\domain.rs:
+v040_memory\src\storage.rs:
+v040_memory\src\storages\mod.rs:
+v040_memory\src\storages\memory.rs:
---
+++ target/debug/deps/v040_memory-9ca526b10f3658cc.d
@@ -0,0 +1,5 @@
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\target\debug\deps\v040_memory-9ca526b10f3658cc.exe: v040_memory\src/main.rs
+
+C:\Users\dodo2\Documents\Ecole\BUT informatique\BUT2\S4\Rust\Bureau_de_vote\target\debug\deps\v040_memory-9ca526b10f3658cc.d: v040_memory\src/main.rs
+
+v040_memory\src/main.rs:
