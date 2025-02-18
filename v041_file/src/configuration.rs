da7266917b1e3236d54a250511f5e85ad1261300
Hashual <loizeau.dorian05@gmail.com>
2025-02-18 15:58:06 +0100

    End of TP 4

--- v041_file/src/configuration.rs
+++ v041_file/src/configuration.rs
@@ -1,7 +1,17 @@
 use clap::Parser;
+use clap::ValueEnum;
 
 #[derive(Parser)]
 pub struct Configuration {
     #[arg(short, long, required = true, num_args = 1.. )]
     pub candidats: Vec<String>,
+
+    #[arg(short, long, default_value_t = StorageType::Memory, value_enum)]
+    pub storage: StorageType,
+}
+
+#[derive(Clone,Copy, ValueEnum)]
+pub enum StorageType {
+    File,
+    Memory,
 }
